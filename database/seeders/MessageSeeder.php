f9f1aa392c7661ce9923c00f094b8774ba64471a
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-26 10:53:15 +0200

    Create Message Seeder (COMMENTATO ATTENDI APPART.)

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,11 @@ class MessageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // $message = new Message();
+        // $message->name = 'Mario';
+        // $message->surname = 'Rossi';
+        // $message->email = 'mariorossi@gmail.com';
+        // $message->content = 'Ciao sarei interessato all\'appartamento ci possiamo sentire?';
+        // $message->save();
     }
 }
