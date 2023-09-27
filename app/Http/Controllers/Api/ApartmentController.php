0295f87130f2db44dd733cd63d046a53ca0e7d3d
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-27 11:59:10 +0200

    add update method in api apartment controller

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -7,7 +7,7 @@ use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
-
+use Illuminate\Validation\Rule;
 
 class ApartmentController extends Controller
 {
@@ -96,7 +96,54 @@ class ApartmentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data_apartment = $request->all();
+        $request->validate(
+            [
+                'user_id' => 'required|string|exists:users,id',
+                'title' => ['required', 'string', Rule::unique('apartments', 'title')],
+                'description' => 'required|string',
+                'address' => 'required|string',
+                'longitude' => 'nullable|string',
+                'latitude' => 'nullable|string',
+                'image' => 'nullable|image',
+                'beds' => 'required|integer',
+                'rooms' => 'nullable|integer',
+                'bathrooms' => 'nullable|integer',
+                'square_meters' => 'nullable|integer',
+                'is_visible' => 'required|boolean',
+            ],
+            [
+                'user_id.required' => 'E\' necessario possedere un\' account per la registrazione',
+                'user_id.exists' => 'l\' utente non esiste',
+                'title.required' => 'Il titolo è obbligatorio',
+                'description.required' => 'La descrizione è obbligatoria',
+                'address.required' => 'L\' indirizzo è obbligatorio',
+                'image.image' => 'Il file inserito non è un immagine',
+                'beds.require' => 'Il numero di posti letto è obbligatorio',
+                'beds.integer' => 'Valore inserito non numerico',
+                'rooms.integer' => 'Valore inserito non numerico',
+                'bathrooms.integer' => 'Valore inserito non numerico',
+                'square_meters.integer' => 'Valore inserito non numerico',
+                'is_visible.required' => 'La disponibilità è obbligatoria',
+                'is_visible.boolean' => 'Valore inserito non valido',
+            ]
+        );
+        $apartment = Apartment::where('id', "$id")->first();
+        if (Arr::exists($data_apartment, 'image')) {
+            if ($apartment->image) Storage::delete($apartment->image);
+            $img_url = Storage::putFile('apartment_images', $data_apartment['image']);
+            $data_apartment['image'] = $img_url;
+        }
+        $apartment->user_id = ($data_apartment['user_id']);
+        $apartment->title = ($data_apartment['title']);
+        $apartment->description = ($data_apartment['description']);
+        $apartment->address = ($data_apartment['address']);
+        $apartment->beds = ($data_apartment['beds']);
+        $apartment->is_visible = ($data_apartment['is_visible']);
+        $apartment->update($data_apartment);
+
+
+        return response()->json($apartment);
     }
 
     /**
