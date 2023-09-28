83457d8f5d20451245a44d7ab466ef1a9cf6465a
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-28 11:42:18 +0200

    Add Views for Admin Apartment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ApartmentController as AdminApartmentController;
 use App\Http\Controllers\ApartmentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -30,3 +31,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::resource('admin/appartaments', AdminApartmentController::class);
