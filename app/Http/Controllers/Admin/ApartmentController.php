8c05abba9e9f645551acd1981e5a57784d5920bc
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-28 13:05:48 +0200

    Add all apartments for only user

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class ApartmentController extends Controller
@@ -14,8 +15,9 @@ class ApartmentController extends Controller
      */
     public function index()
     {
-        $apartments = Apartment::all();
-        return view('admin.apartments.index', compact('apartments'));
+        $user = Auth::user();
+        $apartments = Apartment::where('user_id', $user->id)->get();
+        return view('admin.apartments.index', compact('apartments', 'user'));
     }
 
     /**
