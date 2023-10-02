6fb20af7bae2affcdebe7dee8d5d2d3aa8f849c8
AndreaRomano02 <andy.romano2002@gmail.com>
2023-10-02 17:34:36 +0200

    Move dashboard on navbar and add comment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,5 @@
 @extends('layouts.dashboard')
+
+@section('content')
+    <h1 class="my-5 text-center">Benvenuto nella tua Dashboard</h1>
+@endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -21,10 +21,11 @@
 
         <div class="row g-0">
             <div class="col-3 columns-sx">
+
+                {{-- # Account Name --}}
                 <div class="container left-col">
                     <a class="dropdown-item my-4" href="{{ url('profile') }}">
                         <div class="d-flex align-items-start">
-
                             <span class="material-symbols-outlined dash-icon">
                                 account_circle
                             </span>
@@ -34,24 +35,7 @@
                         </div>
                     </a>
 
-                    <div class="card my-3">
-
-                        <div class="card-body">
-
-                            <a class="btn btn-outline-secondary" href="{{ route('admin.apartments.index') }}">
-                                <div class="d-flex align-items-cente justify-content-start">
-                                    <span class="material-symbols-outlined me-2">
-                                        apartment
-                                    </span>
-
-                                    <h6 class="pt-1 mb-0">
-                                        Vai ai tui Appartamenti
-                                    </h6>
-                                </div>
-                            </a>
-
-                        </div>
-                    </div>
+                    {{-- # Aggiungi un appartamento --}}
                     <div class="card my-3">
                         <div class="card-body">
                             <a href="{{ route('admin.apartments.create') }}" class="btn btn-success">
@@ -61,19 +45,34 @@
                                         add
                                     </span>
                                     <h6 class="pt-1 mb-0">
-                                        Aggiungi un'appartamento
+                                        Aggiungi un appartamento
                                     </h6>
                                 </div>
                             </a>
                         </div>
-
                     </div>
-                    <div class="card my-3">
 
+                    {{-- # Vai ai tui Appartamenti --}}
+                    <div class="card my-3">
+                        <div class="card-body">
+                            <a class="btn btn-outline-secondary" href="{{ route('admin.apartments.index') }}">
+                                <div class="d-flex align-items-cente justify-content-start">
+                                    <span class="material-symbols-outlined me-2">
+                                        apartment
+                                    </span>
+                                    <h6 class="pt-1 mb-0">
+                                        Vai ai tui Appartamenti
+                                    </h6>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    {{-- # Vai all'archivio --}}
+                    <div class="card my-3">
                         <div class="card-body">
                             <a href="{{ route('admin.apartments.archive') }}" class="btn btn-secondary">
                                 <div class="d-flex align-items-cente justify-content-start">
-
                                     <span class="material-symbols-outlined me-2">
                                         archive
                                     </span>
@@ -83,10 +82,12 @@
                                 </div>
                             </a>
                         </div>
-
                     </div>
+
                 </div>
             </div>
+
+            {{-- # Content --}}
             <div class="col-9 columns-dx">
                 <div class="right-col">
                     <div class="container">
