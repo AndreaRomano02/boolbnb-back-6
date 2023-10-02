76c942bfcf526e6945ba46b043f2e12364a651e6
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-10-02 14:24:19 +0200

    fix registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,14 +30,34 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // dd($request);
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'name' => ['string', 'max:20', 'min:3'],
+            'surname' => ['string', 'nullable'],
+            'date_birth' => ['date', 'nullable'],
+            'email' => ['required', 'string', 'email', 'min:10', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ], [
+            'name.string' => 'L\' Elemento inserito non è testo',
+            'name.max' => 'Il Testo inserito è troppo lungo massimo 20 caratteri',
+            'name.min' => 'Il Testo inserito è troppo corto minimo 3 caratteri',
+            'surname.string' => 'L\' Elemento inserito non è testo',
+            'date_birth.date' => 'L\' Elemento inserito non è una data',
+            'email.required' => 'L\' Email è obbligatoria',
+            'email.email' => 'L\' Elemento inserito non è un\' Email',
+            'email.max' => 'L\' Email inserita è troppo lungo massimo 255 caratteri',
+            'email.min' => 'L\' Email inserita è troppo corta massimo 10 caratteri',
+            'email.unique' => 'L\'Email esiste già',
+            'password.required' => 'La password è obbligatoria',
+            'password.min' => 'La password deve essere di minimo 8 caratteri',
+            'password.confirmed' => 'Le password deveno essere identiche',
+
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'surname' => $request->surname,
+            'date_birth' => $request->date_birth,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('date_birth');
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->date('date_birth')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
